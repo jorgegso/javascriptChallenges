50539fa5ea41bb35b997dc689947216b044d362b
mikiimikii <hajorge2012@gmail.com>
2020-07-07 07:21:40 -0300

    adding files

---
+++ jsprac.js
@@ -0,0 +1,21 @@
+var contenido = document.querySelector('#contenido')
+
+
+function traer() {
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results['0'].email)
+      contenido.innerHTML = `
+      <div class="flex-container ">
+        <img src="${data.results['0'].picture.large}" width="200px">
+        <p>Nombre: ${data.results['0'].name.last} </p>
+      </div>`
+    })
+
+}
+
+
+
+
+
